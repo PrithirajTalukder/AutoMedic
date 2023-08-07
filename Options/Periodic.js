17c18162a30fd0a9ce4de97b3ba12d37755b39f3
Musaddek Ahmed <113886332+Musaddek-Ahmed@users.noreply.github.com>
2023-08-07 21:59:15 +0600

    latest push  Changes to be committed: 	modified:   App.js 	modified:   Options/Periodic.js 	modified:   Screen/Help.js 	modified:   Screen/Market.js 	new file:   images/workInP.png

--- Options/Periodic.js
+++ Options/Periodic.js
@@ -1,11 +1,41 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, Text, View,Image,TouchableOpacity } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/native";
 
 const Periodic =()=>{
+    const navigation = useNavigation();
     return (
-        <View>
-            <Text style={{ color: "blue",fontSize:25,}}>you are welcome</Text>
-        </View>
+        <SafeAreaView style={{flex:1}}>
+
+           <View style={{
+            flexDirection: "row",
+            top:60,
+            paddingLeft:23
+            }}>
+            
+            <TouchableOpacity onPress={() => navigation.navigate("Main")}><AntDesign name="arrowleft" size={24} color="black" /></TouchableOpacity>
+            <Text style={{paddingLeft:22,fontSize:18, fontWeight:600}}>Back</Text>
+            </View>
+
+            <View style={{backgroundColor:"white",
+            borderRadius:20,
+            top:100,
+            width:350,
+            paddingLeft:20,
+            paddingRight:20,
+            marginLeft:20,
+            marginRight:10,
+            height:670,
+            position: "absolute",
+            backgroundColor:"lightblue",
+            elevation:6,
+              }}>
+            <Image source={require("../images/workInP.png")}
+            style={{ width: 110, height: 110,marginTop:250, marginLeft:100 }} />
+
+              </View>
+        </SafeAreaView>
     )
 }
 
--- Screen/Help.js
+++ Screen/Help.js
@@ -1,12 +1,34 @@
 import React from 'react'
-import { Text, View } from 'react-native'
+import { SafeAreaView, Text, View,Image,TouchableOpacity } from 'react-native'
+import { AntDesign } from '@expo/vector-icons';
+import { useNavigation } from "@react-navigation/native";
 
-const Help =()=>{
+const Periodic =()=>{
+    const navigation = useNavigation();
     return (
-        <View>
-            <Text style={{ color: "blue",fontSize:25,}}>you are welcome</Text>
-        </View>
+        <SafeAreaView style={{flex:1}}>
+
+           
+
+            <View style={{backgroundColor:"white",
+            borderRadius:20,
+            top:80,
+            width:350,
+            paddingLeft:20,
+            paddingRight:20,
+            marginLeft:20,
+            marginRight:10,
+            height:670,
+            position: "absolute",
+            backgroundColor:"lightblue",
+            elevation:6,
+              }}>
+            <Image source={require("../images/workInP.png")}
+            style={{ width: 110, height: 110,marginTop:250, marginLeft:100 }} />
+
+              </View>
+        </SafeAreaView>
     )
 }
 
-export default Help
+export default Periodic
