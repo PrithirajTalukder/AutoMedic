591da260fbae5a4e5a02f0064dd9a46d15f6d217
PrithirajTaklukder <prithirajtalukder@gmai.com>
2024-01-24 01:54:48 +0600

    Changes to be committed: 	modified:   Screen/Chat.js 	modified:   package-lock.json 	modified:   package.json

--- Screen/Chat.js
+++ Screen/Chat.js
@@ -1,12 +1,20 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import React, { useState } from 'react';
 import { GiftedChat } from 'react-native-gifted-chat';
 
 const Chat = () => {
+  const [messages, setMessages] = useState([]);
+
+  const onSend = (newMessages = []) => {
+    setMessages((prevMessages) => GiftedChat.append(prevMessages, newMessages));
+  };
+
   return (
-    
-      <GiftedChat/>
-    
-  )
-}
+    <GiftedChat
+      messages={messages}
+      onSend={(newMessages) => onSend(newMessages)}
+      user={{ _id: 1 }} // User ID, you can modify this based on your user system
+    />
+  );
+};
+
 export default Chat;
