36f31c3b45b60ab06c3928f98c9f775587d229f4
Musaddek Ahmed <113886332+Musaddek-Ahmed@users.noreply.github.com>
2024-01-30 00:40:52 +0600

    updated 	modified:   Options/Periodic.js

--- Options/Periodic.js
+++ Options/Periodic.js
@@ -152,8 +152,7 @@ const Periodic = () => {
             visible={isFilterModalVisible}
             onRequestClose={closeModal}
           >
-<<<<<<< HEAD
-                <View style={styles.modalContainer}>
+          <View style={styles.modalContainer}>
             <View style={styles.modalBox}>
             <TouchableOpacity onPress={closeModal} style={{marginLeft:270}}>
                 <FontAwesome name="times-circle" size={28} color="black" />
@@ -180,48 +179,15 @@ const Periodic = () => {
                 <TouchableOpacity onPress={() => applyFilter(null)} 
                 style={{ marginTop:60,width:'40%', backgroundColor:'#15174f',height:'9%',justifyContent:'center', alignItems:'center', borderRadius:4,}}>
                   <Text style={{color:'white', fontSize: 14, fontWeight: 600, }}>Clear Filter</Text>
-=======
-            <View style={styles.modalContainer}>
-              <View style={styles.modalBox}>
-                <TouchableOpacity onPress={closeModal} style={{ marginLeft: 250 }}>
-                  <FontAwesome name="times-circle" size={28} color="black" />
->>>>>>> 11d22ac86da3d35dca2f8256887f2f521fe704ab
                 </TouchableOpacity>
-                <Text style={styles.modalTitle}>Filter Modal</Text>
-
-                <View style={styles.modalOptions}>
-                  <TouchableOpacity onPress={() => applyFilter('name')}
-                    style={{ marginTop: 25, backgroundColor: 'white', width: '90%', height: '10%', justifyContent: 'center', borderRadius: 5, alignItems: 'center' }}>
-                    <Text style={{ color: "black", fontSize: 17, fontWeight: 600 }}>Sort By Name</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={() => applyFilter('lowToHighPrice')}
-                    style={{ marginTop: 25, backgroundColor: 'white', width: '90%', height: '10%', justifyContent: 'center', borderRadius: 5, alignItems: 'center' }}>
-                    <Text style={{ color: "black", fontSize: 17, fontWeight: 600 }}>Low to High Price</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={() => applyFilter('highToLowPrice')}
-                    style={{ marginTop: 25, backgroundColor: 'white', width: '90%', height: '10%', justifyContent: 'center', borderRadius: 5, alignItems: 'center' }}>
-                    <Text style={{ color: "black", fontSize: 17, fontWeight: 600 }}>High to Low Price</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={() => applyFilter('rating')}
-                    style={{ marginTop: 25, backgroundColor: 'white', width: '90%', height: '10%', justifyContent: 'center', borderRadius: 5, alignItems: 'center' }}>
-                    <Text style={{ color: "black", fontSize: 17, fontWeight: 600 }}>Sort By Rating</Text>
-                  </TouchableOpacity>
-                  <TouchableOpacity onPress={clearFilters}
-                    style={{ marginTop: 60, width: '40%', backgroundColor: 'white', height: '7%', justifyContent: 'center', alignItems: 'center', borderRadius: 4 }}>
-                    <Text style={{ fontSize: 14, fontWeight: 600 }}>Clear Filter</Text>
-                  </TouchableOpacity>
-                </View>
+                
               </View>
+            </View>
             </View>
           </Modal>
 
-<<<<<<< HEAD
           <View style={{ marginLeft: 25, marginTop: 10, top: 20, marginBottom: 20 }}>
             <Text style={{ color: "black", fontSize: 24, fontWeight: 800, }}>
-=======
-          <View style={{ marginLeft: 25, marginTop: 10, top: 20, marginBottom: 40 }}>
-            <Text style={{ color: "black", fontSize: 24, fontWeight: 800 }}>
->>>>>>> 11d22ac86da3d35dca2f8256887f2f521fe704ab
               Scheduled Packages
             </Text>
           </View>
