e3ea79456732630ce3c40587c8fdfb2a782052c1
Musaddek Ahmed <113886332+Musaddek-Ahmed@users.noreply.github.com>
2024-02-18 22:42:17 +0600

    pushed 	new file:   Products/Lights.js 	modified:   Screen/Market.js 	new file:   images/products/front1.jpg 	new file:   images/products/front2.jpg 	new file:   images/products/front3.jpg 	new file:   images/products/front4.jpg

--- App.js
+++ App.js
@@ -25,6 +25,7 @@ import Mechanic from './Screen/Mechanic';
 import Account from './Screen/Account';
 import Profile from './Screen/Profile';
 import Batteries from './Products/Batteries';
+
 import MyCart from './Screen/MyCart';
 import Preparingorder from './Screen/Preparingorder';
 import DeliveryScreen from './Screen/DeliveryScreen';
@@ -33,6 +34,7 @@ import Payment from './Screen/Payment';
 import Chat from './Screen/Chat';
 
 import { StripeProvider } from '@stripe/stripe-react-native';
+import Lights from './Products/Lights';
 
 
 
@@ -71,6 +73,7 @@ const App = () => {
           <Stack.Screen name="Account" options={{ headerShown: false }} component={Account} />
           <Stack.Screen name="Mechanic" options={{ headerShown: false }} component={Mechanic} />
           <Stack.Screen name="Batteries" options={{ headerShown: false }} component={Batteries} />
+          <Stack.Screen name="Lights" options={{ headerShown: false }} component={Lights} />
           <Stack.Screen name="MyCart" options={{ headerShown: false }} component={MyCart} />
           <Stack.Screen name="Preparingorder" options={{ headerShown: false }} component={Preparingorder} />
           <Stack.Screen name="DeliveryScreen" options={{ headerShown: false }} component={DeliveryScreen} />
